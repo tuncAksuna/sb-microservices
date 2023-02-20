2d4fe140ef23869d42ae13086fec74846ee36172
caksuna <caksuna@innova.com.tr>
2023-02-20 14:09:00 +0300

    RestTemplate used for inter-service communication

--- currency-conversion-service/src/main/java/com/tuncode/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/tuncode/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,10 +1,13 @@
 package com.tuncode.microservices.currencyconversionservice;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
 @RestController
 public class CurrencyConversionController {
@@ -15,7 +18,22 @@ public class CurrencyConversionController {
                                                           @PathVariable String to,
                                                           @PathVariable BigDecimal quantity) {
 
-        return new CurrencyConversion(10001L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "");
+        // using to make API calls (send request from CurrencyConversion to CurrencyExchange)
+        HashMap<String, String> uriVariable = new HashMap<>();
+        uriVariable.put("from", from);
+        uriVariable.put("to", to);
+
+        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate()
+                .getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariable);
+
+        return new CurrencyConversion(
+                responseEntity.getBody().getId(),
+                from,
+                to,
+                responseEntity.getBody().getQuantity(),
+                responseEntity.getBody().getConversionMultiple(),
+                quantity.multiply(responseEntity.getBody().getConversionMultiple()),
+                responseEntity.getBody().getEnvironment());
     }
 
 }
