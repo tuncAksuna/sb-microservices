d222f4f367dee25bc8ebed4733e691a72333f802
caksuna <caksuna@innova.com.tr>
2023-02-27 21:20:36 +0300

    added netflix eureka server and changed configuration of config server

--- currency-conversion-service/src/main/java/com/tuncode/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/tuncode/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -4,9 +4,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.math.BigDecimal;
 
-@FeignClient(value = "currency-exchange", url = "localhost:8000")
+//@FeignClient(value = "currency-exchange", url = "localhost:8000")
+@FeignClient(value = "currency-exchange") // Deleted url parameter for Netlix Eureka Automatic Load Balancing
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
