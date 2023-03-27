458cff6382a7cda400290887d184e7484b728f24
caksuna <caksuna@innova.com.tr>
2023-03-27 10:00:47 +0300

    circut breaker

--- currency-exchange-service/src/main/java/com/tuncode/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/tuncode/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,7 +1,10 @@
 package com.tuncode.microservices.currencyexchangeservice;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +18,7 @@ public class CurrencyExchangeController {
 
     private final CurrencyExchangeRepository repository;
     private final Environment environment;
-    private static final CURRENCY_EXCHANGE_INSTANCE ="currencyExchangeService"
+    private static final String CURRENCY_EXCHANGE_INSTANCE = "currencyExchangeService";
 
     @Autowired
     public CurrencyExchangeController(CurrencyExchangeRepository repository, Environment environment) {
@@ -24,7 +27,7 @@ public class CurrencyExchangeController {
     }
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
-    @CircuitBreaker(name = CURRENCY_EXCHANGE_INSTANCE, fallBackMethod = "exchangeServiceFallBack")
+    @CircuitBreaker(name = CURRENCY_EXCHANGE_INSTANCE, fallbackMethod = "exchangeServiceFallBack")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
 
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
@@ -41,6 +44,6 @@ public class CurrencyExchangeController {
     }
 
     public ResponseEntity<String> exchangeServiceFallBack(Exception exc) {
-        return ResponseEntity < String > ("Currency Exchange service is down ",HttpStatus.OK);
+        return new ResponseEntity<String> ("Currency Exchange service is down ", HttpStatus.OK);
     }
 }
